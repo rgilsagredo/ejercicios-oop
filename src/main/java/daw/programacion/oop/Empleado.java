b0758c6d90ba635eba815b9d610f0bdaacf28c3f
Raúl Gil de Sagredo <rgilsagredo@gmail.com>
2022-12-20 20:23:10 +0100

    añado constructores, getters, setters, toString a Empleado

--- src/main/java/daw/programacion/oop/Empleado.java
+++ src/main/java/daw/programacion/oop/Empleado.java
@@ -1,11 +1,42 @@
 package daw.programacion.oop;
 
 public class Empleado {
-    
+
     private int id;
     private double salario;
     private String nombre;
     private String apellido;
 
+    public Empleado(int id, double salario, String nombre, String apellido) {
+        this.id = id;
+        this.salario = salario;
+        this.nombre = nombre;
+        this.apellido = apellido;
+    }
+
+    public int getId() {
+        return this.id;
+    }
+
+    public double getSalario() {
+        return this.salario;
+    }
+
+    public String getNombre() {
+        return this.nombre;
+    }
+
+    public String getApellido() {
+        return this.apellido;
+    }
+
+    public void setSalario(double salario) {
+        this.salario = salario;
+    }
+
+    @Override
+    public String toString() {
+        return "Empleado [id=" + id + ", salario=" + salario + ", nombre=" + nombre + ", apellido=" + apellido + "]";
+    }
 
 }
