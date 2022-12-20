badac58c96ffdf90d1bc6018927ab2cde7a962cb
Raúl Gil de Sagredo <rgilsagredo@gmail.com>
2022-12-20 19:51:21 +0100

    añado getters y setters para Circulo.radio

--- src/main/java/daw/programacion/oop/Circulo.java
+++ src/main/java/daw/programacion/oop/Circulo.java
@@ -2,4 +2,21 @@ package daw.programacion.oop;
 
 public class Circulo {
     private double radio = 1.0;
+
+    public Circulo() {
+    }
+
+    public Circulo(double radio) {
+        this.radio = radio;
+    }
+
+    public double getRadio() {
+        return this.radio;
+    }
+
+    public void setRadio(double radio) {
+        this.radio = radio;
+    }
+
+    
 }
